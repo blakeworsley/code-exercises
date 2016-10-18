972d43de68fd18532f58cac66abdba357f569f9c
Blake Worsley <blakeworsley@gmail.com>
2016-10-18 09:50:41 -0600

    added more diverse test

--- src/reverse-string.test.js
+++ src/reverse-string.test.js
@@ -2,4 +2,6 @@ const reverseString = require('./reverse-string');
 
 it('should reverse a string', () => {
   expect(reverseString('hello')).toBe('olleh');
+  expect(reverseString('yo')).toBe('oy');
+  expect(reverseString('1234')).toBe('4321');
 });
