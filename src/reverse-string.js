497f22e29dde3d7c46d8ec377661c064df2c3887
Blake Worsley <blakeworsley@gmail.com>
2016-10-18 10:03:22 -0600

    added optimized solution

--- src/reverse-string.js
+++ src/reverse-string.js
@@ -1,3 +1,16 @@
+'use strict';
+
 module.exports = (string) => {
-  return string.split("").reverse().join("");
+  let result = '';
+  for (let i = string.length - 1; i > -1; i--) {
+    result += string[i];
+  }
+  return result;
 };
+
+
+
+// FIRST PASS
+// module.exports = (string) => {
+//   return string.split("").reverse().join("");
+// };
